3e2b0268cea6fec7aa220a782cb3fceffcb14e96
Mateonidas <teus.polec@gmail.com>
2021-04-24 18:11:02 +0200

    feat: service implementation - end

--- src/app/recipes/recipe-service/recipe.service.ts
+++ src/app/recipes/recipe-service/recipe.service.ts
@@ -1,16 +1,41 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../../shopping-list/shopping-list-service/shopping-list.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Test description', 'https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/05/homemade-birria-tacos-recipe-3273w-1024x683.jpg'),
-    new Recipe('Another Test Recipe', 'Another description', 'https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/05/homemade-birria-tacos-recipe-3273w-1024x683.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'Test description',
+      'https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/05/homemade-birria-tacos-recipe-3273w-1024x683.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+      ]),
+    new Recipe(
+      'Another Test Recipe',
+      'Another description',
+      'https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/05/homemade-birria-tacos-recipe-3273w-1024x683.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('French Meat', 1),
+      ])
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   getRecipes() {
-    return  this.recipes.slice();
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
